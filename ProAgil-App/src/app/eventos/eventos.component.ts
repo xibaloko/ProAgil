33c4f65b5be1ffcb32cac4f2b08b1ab18a73b0fe
Ulisses Inacio <ulissesmilitao@hotmail.com>
2021-07-02 18:47:47 -0300

    Adicionando http request

--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -1,33 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-eventos',
   templateUrl: './eventos.component.html',
-  styleUrls: ['./eventos.component.css']
+  styleUrls: ['./eventos.component.css'],
 })
 export class EventosComponent implements OnInit {
+  eventos: any;
 
-  eventos : any = [
-    {
-      EventoId: 1,
-      Tema: 'Angular',
-      Local: 'Belo Horizonte'
-    },
-    {
-      EventoId: 2,
-      Tema: '.NET Core',
-      Local: 'São Paulo'
-    },
-    {
-      EventoId: 3,
-      Tema: 'Angular e .NET Core',
-      Local: 'Rio de Janeiro'
-    }
-  ];
-
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
+    this.getEventos();
   }
 
+  getEventos() {
+    this.http.get('http://localhost:5000/api/values').subscribe(
+      (response) => {
+        this.eventos = response;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
--- ProAgil.WebApi/Startup.cs
+++ ProAgil.WebApi/Startup.cs
@@ -27,8 +27,12 @@ namespace ProAgil.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<DataContext>(
+                x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection"))
+            );
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +49,7 @@ namespace ProAgil.WebApi
             }
 
             //app.UseHttpsRedirection();
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseMvc();
         }
     }
